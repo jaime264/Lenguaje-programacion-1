b2caf5e03afe518e0c1a64ead7ddfc950a577e4b
jaime <jaime264@gmail.com>
2023-12-02 20:22:32 -0500

    jsf clase 1

---
+++ src/main/java/demo_sesion_01/model/Profesor.java
@@ -0,0 +1,76 @@
+package demo_sesion_01.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profesor")
+public class Profesor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column
+	private String nombre;
+
+	@Column
+	private String apellido;
+
+	@Column
+	private int telefono;
+
+	@Column
+	private String carrera;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCarrera() {
+		return carrera;
+	}
+
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+
+	@Override
+	public String toString() {
+		return "Profesor [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono
+				+ ", carrera=" + carrera + "]";
+	}
+
+}
---
+++ src/main/java/demo_sesion_01/util/JPAUtil.java
@@ -0,0 +1,21 @@
+package demo_sesion_01.util;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAUtil {
+	
+	private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE";
+	private static EntityManagerFactory factory;
+	
+	public static EntityManagerFactory getEntityManagerFactory() {
+		if(factory == null) 
+			factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		return factory;
+	}
+	
+	public static void shutdown() {
+		if(factory != null)
+			factory.close();
+	}
+}
